f33f5dcb08aa46f272da3a0e18481fefe475d4ac
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-18 15:50:40 +0530

    Update start.py

--- xDspam/bot/start.py
+++ xDspam/bot/start.py
@@ -1,24 +1,24 @@
 # ""ᴅᴇᴀʀ ᴘʀᴏ ᴘᴇᴏᴘʟᴇ,  ᴅᴏɴ'ᴛ ʀᴇᴍᴏᴠᴇ & ᴄʜᴀɴɢᴇ ᴛʜɪꜱ ʟɪɴᴇ
-# ᴛɢ :- @FabinoXD
-# ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ :- @xDspamBots
-# ɢɪᴛʜᴜʙ :- @FabinoXD ""
+# ᴛɢ :- @GhostRadha
+# ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ :- @RadhaX2Support
+# ɢɪᴛʜᴜʙ :- @GhostRadha ""
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from xDspam import ALIVE_PIC
 
-Pic = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/a50a32854921ea49b665f.jpg"
+Pic = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/84351002f7dd488a17abb.jpg"
 Hn = "/"
 btn = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("• Channel •", url="https://t.me/EnoughBio"),
-            InlineKeyboardButton("• Support •", url="https://t.me/DevilsHeavenMF"),
+            InlineKeyboardButton("• Channel •", url="https://t.me/RadhaX2Update"),
+            InlineKeyboardButton("• Support •", url="https://t.me/RadhaX2Support"),
         ],
         [
             InlineKeyboardButton(
-                "• Repo •", url="https://github.com/FabinoXD/xDspam"
+                "• Repo •", url="https://github.com/Radhak8/RadhaX2Spam"
             )
         ],
     ]
@@ -29,7 +29,7 @@ btn = InlineKeyboardMarkup(
     filters.private & filters.incoming & filters.command(["start"], prefixes=Hn)
 )
 async def _start(_, ok: Message):
-    msg = f"**Hello [{ok.from_user.first_name}](tg://user?id={ok.from_user.id}) !** \n\n __ • I'm xDspam An Advance Spambot__ \n\n **Click Below Buttons for More Info**"
+    msg = f"**Hello [{ok.from_user.first_name}](tg://user?id={ok.from_user.id}) !** \n\n __ • I'm Radha An Advance Spambot__ \n\n **Click Below Buttons for More Info**"
     if ".jpg" in Pic or ".png" in Pic:
         await ok.reply_photo(Pic, caption=msg, reply_markup=btn)
     if ".mp4" in Pic or ".MP4," in Pic:
